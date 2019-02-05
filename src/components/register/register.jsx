92c7a9749d75668e5f7b8ff99e3694a4e58f221e
rdtharindudk <dilshantharinduk@gmail.com>
2019-02-06 02:01:41 +0530

    bug fixes

--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -42,77 +42,84 @@ class Register extends Component {
 
   render() {
     return (
-      <div className="text-center">
-        <form className="form-register" onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="firstname">First Name</label>
-            <input
-              type="text"
-              className="form-control"
-              id="firstname"
-              placeholder="Enter First Name"
-              name="firstname"
-              value={this.state.account.firstname}
-              onChange={this.handleChange}
-            />
+      <div className="container down form-register">
+        <div className="container down">
+          <div class="card">
+            <div class="card-header">Register</div>
+            <div class="card-body">
+              <form className="" onSubmit={this.handleSubmit}>
+                <div className="form-group">
+                  <label htmlFor="firstname">First Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="firstname"
+                    placeholder="Enter First Name"
+                    name="firstname"
+                    value={this.state.account.firstname}
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="lastname">Last Name</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="lastname"
+                    placeholder="Enter Last Name"
+                    name="lastname"
+                    value={this.state.account.lastname}
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="email">Email address</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    aria-describedby="emailHelp"
+                    placeholder="Enter email"
+                    name="email"
+                    value={this.state.account.email}
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">Password</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    placeholder="Password"
+                    name="password"
+                    value={this.state.account.password}
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="confirmpassword">Confirm Password</label>
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="confirmpassword"
+                    placeholder="Confirm Password"
+                    name="confirmpassword"
+                    value={this.state.account.confirmpassword}
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <button
+                  disabled={!this.validate()}
+                  type="submit"
+                  className="btn btn-primary"
+                >
+                  Register
+                </button>
+              </form>
+            </div>
           </div>
-          <div className="form-group">
-            <label htmlFor="lastname">Last Name</label>
-            <input
-              type="text"
-              className="form-control"
-              id="lastname"
-              placeholder="Enter Last Name"
-              name="lastname"
-              value={this.state.account.lastname}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="email">Email address</label>
-            <input
-              type="email"
-              className="form-control"
-              id="email"
-              aria-describedby="emailHelp"
-              placeholder="Enter email"
-              name="email"
-              value={this.state.account.email}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              className="form-control"
-              id="password"
-              placeholder="Password"
-              name="password"
-              value={this.state.account.password}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="confirmpassword">Confirm Password</label>
-            <input
-              type="password"
-              className="form-control"
-              id="confirmpassword"
-              placeholder="Confirm Password"
-              name="confirmpassword"
-              value={this.state.account.confirmpassword}
-              onChange={this.handleChange}
-            />
-          </div>
-          <button
-            disabled={!this.validate()}
-            type="submit"
-            className="btn btn-primary"
-          >
-            Register
-          </button>
-        </form>
+        </div>
       </div>
     );
   }
